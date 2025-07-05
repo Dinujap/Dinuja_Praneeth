23c6a1ea6767de6096657967d2f8746a61260f15
Dinuja Praneeth <dinujapraneeth76@gmail.com>
2025-07-05 22:17:41 +0530

    create the css and update folders components, sections and constant

---
+++ app/Constant/Index.tsx
@@ -0,0 +1,7 @@
+export const words = [
+    { text: "Ideas", imgPath: "/Images/ideas.svg" },
+    { text: "Concepts", imgPath: "/Images/concepts.svg" },
+    { text: "Designs", imgPath: "/Images/designs.svg" },
+    { text: "Code", imgPath: "/Images/code.svg" },
+]
+   
---
+++ app/Sections/Hero.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { words } from '../Constant/Index';
+
+
+const Hero = () => {
+  return (
+    <section id='hero' className='relative overflow-hidden'>
+        <div className='absolute top-0 left-0 z-10'>
+            <img src="/Images/bg.png" alt='background'></img>            
+        </div>
+        <div className='hero-layout'>
+          <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
+            <div className='hero-text'>
+              <h1>
+                Shaping
+                <span className='slide'>
+                  <span className='wrapper'>
+                    {words.map((word) => (
+                      <span key={word.text} className='flex items-center md:gap-3 gap-1 pb-2'> 
+                        <img src={word.imgPath} alt={word.text} className='xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50'>
+
+                        </img>
+                        <span>
+                          {word.text}
+                        </span>
+
+                      </span>
+                    ))}
+                  </span>
+                </span>
+              </h1>
+              <h1>into real projects</h1>
+              <h1>that deliver results</h1>
+            </div>
+          </header>
+        </div>
+    </section>
+    
+  )
+}
+
+export default Hero
